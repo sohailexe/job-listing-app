3ebcc89d93d1ab20f09e6ba51b8109b0900033fe
Sohail Faiz Rasool <sa472811@gmail.com>
2024-10-01 20:23:54 +0500

    add job posting form

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,45 @@
-import React from 'react'
-import {Route, createBrowserRouter,createRoutesFromElements,RouterProvider } from 'react-router-dom'
-import Home from './pages/Home'
-import MainLayout from './layouts/MainLayout'
-import JobsPage from './pages/JobsPage'
-import NotFoundPage from './pages/NotFoundPage'
-import JobPage, {jobLoader} from './pages/JobPage'
-
-const router  = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<MainLayout />} >
-      <Route index element={<Home />} /> loader={jobLoader}
-      <Route path='/jobs' element={<JobsPage />} />
-      <Route path='/jobs/:id' element={<JobPage /> }  loader={jobLoader}/>
-      <Route path='*' element={<NotFoundPage />} />
-
-      
-
-    </Route>
-  )
-
-)
+import React from "react";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import MainLayout from "./layouts/MainLayout";
+import JobsPage from "./pages/JobsPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import JobPage, { jobLoader } from "./pages/JobPage";
+import AddJobPage from "./pages/AddJobPage";
 
 const App = () => {
-  return (
-     <RouterProvider router={router}/>
-  )
-}
- 
-export default App
+  // utillity functions
+  const addJobSubmit = async (newJob) => {
+    const res = await fetch("/api/jobs", {
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(newJob),
+    });
+    return;
+  };
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Home />} /> loader={jobLoader}
+        <Route path="/jobs" element={<JobsPage />} />
+        <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader} />
+        <Route
+          path="/add-job"
+          element={<AddJobPage addJobSubmit={addJobSubmit} />}
+        />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    )
+  );
+
+  return <RouterProvider router={router} />;
+};
+
+export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,18 @@
-import React from 'react'
-import Hero from '../components/Hero'
-import HomeCards from '../components/HomeCards'
-import JobListings from '../components/JobListings'
-import ViewAllJobs from '../components/ViewAllJobs'
+import React from "react";
+import Hero from "../components/Hero";
+import HomeCards from "../components/HomeCards";
+import JobListings from "../components/JobListings";
+import ViewAllJobs from "../components/ViewAllJobs";
 
 const Home = () => {
   return (
     <>
+      <Hero />
+      <HomeCards />
+      <JobListings isHome={true} />
+      <ViewAllJobs />
+    </>
+  );
+};
 
-    <Hero/>
-    <HomeCards/>
-    <JobListings isHome={true}/>
-    <ViewAllJobs/>
-  </>
-  )
-}
-
-export default Home
+export default Home;
